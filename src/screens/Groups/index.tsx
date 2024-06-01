ac602012e94dfddf99120ec676b6325284c527a9
Felipe Faustini <felipefaustini@MacBook-Pro-2.local>
2024-06-01 09:28:19 -0300

    added navigation and safe area to app

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,22 @@
 import { useState } from 'react';
 import { FlatList } from 'react-native';
+import { Container } from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 import { Hightlight } from '@components/Highlight';
 import { Header } from '@components/Header';
 import { GroupCard } from '@components/GroupCard';
-import { Container } from './styles';
 import { EmptyList } from '@components/EmptyList';
 import { Button } from '@components/Button';
 
 export function Groups() {
   const [groups, setGroups] = useState([]);
+
+  const navigation = useNavigation();
+
+  function handleNewGroup() {
+    navigation.navigate('new');
+  }
 
   return (
     <Container>
@@ -26,7 +33,10 @@ export function Groups() {
         ListEmptyComponent={() => <EmptyList message={'Nenhuma turma encontrada'} />}
       />
 
-      <Button title={'Criar turma'} />
+      <Button 
+        title={'Criar turma'} 
+        onPress={handleNewGroup}
+      />
     </Container>
   );
 }
--- src/screens/NewGroups/index.tsx
+++ src/screens/NewGroups/index.tsx
@@ -1,10 +1,19 @@
+import { Container, Content, Icon } from './styles';
+import { useNavigation } from '@react-navigation/native';
+
 import { Header } from '@components/Header';
 import { Hightlight } from '@components/Highlight';
 import { Button } from '@components/Button';
-import { Container, Content, Icon } from './styles';
 import { Input } from '@components/Input';
 
 export function NewGroup() {
+
+    const navigation = useNavigation();
+
+    function handleNewGroup() {
+        navigation.navigate('players', { group: 'rocket' });
+    }
+
     return (
         <Container >
             <Header showBackButton />
@@ -19,7 +28,9 @@ export function NewGroup() {
                 />
                 <Button
                     title='Criar turma'
-                    style={{ marginTop: 20 }} />
+                    style={{ marginTop: 20 }} 
+                    onPress={handleNewGroup}
+                />
             </Content>
         </Container>
     )
